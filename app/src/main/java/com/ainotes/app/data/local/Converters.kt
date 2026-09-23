22df8944f77efa33db4f040f9c9e06f33a9da566
mosikh98 <khavajehmostafa29@gmail.com>
2026-09-23 12:53:06 +0330

    Delete app/src/main/java/com/ainotes/app directory

--- app/src/main/java/com/ainotes/app/data/local/Converters.kt
+++
@@ -1,8 +0,0 @@
-package com.ainotes.app.data.local
-
-import androidx.room.TypeConverter
-
-class Converters {
-    @TypeConverter fun fromBool(value: Boolean): Int = if (value) 1 else 0
-    @TypeConverter fun toBool(value: Int): Boolean = value != 0
-}
